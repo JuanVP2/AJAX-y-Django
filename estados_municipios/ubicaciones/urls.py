7510abef38574d749fce8c46861dabdae3bea4c1
Juan <Juan@DESKTOP-JVP>
2024-10-19 10:32:17 -0600

    Ejemplo final

--- estados_municipios/ubicaciones/urls.py
+++ estados_municipios/ubicaciones/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import cargar_municipios, index
 
 urlpatterns = [
-    path('ajax/cargar-municipios/', cargar_municipios, name='ajax_cargar_municipios'),
     path('', index, name='index'),
     path('ajax/cargar-municipios/', cargar_municipios, name='ajax_cargar_municipios'),
 ]
--- estados_municipios/ubicaciones/views.py
+++ estados_municipios/ubicaciones/views.py
@@ -1,16 +1,11 @@
-from django.shortcuts import render
-
 from django.http import JsonResponse
-from .models import Municipio
 from django.shortcuts import render
-from .models import Estado
-
+from .models import Municipio, Estado
 
 def cargar_municipios(request):
     estado_id = request.GET.get('estado_id')
     municipios = Municipio.objects.filter(estado_id=estado_id).all()
     return JsonResponse(list(municipios.values('id', 'nombre')), safe=False)
-
 def index(request):
     estados = Estado.objects.all()
     return render(request, 'index.html', {'estados': estados})
