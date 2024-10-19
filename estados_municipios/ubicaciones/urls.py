0da72f30b58f2e9c973de0bc489254a1a6a2ddb4
Juan <Juan@DESKTOP-JVP>
2024-10-19 10:23:33 -0600

    Creacion html

--- estados_municipios/ubicaciones/urls.py
+++ estados_municipios/ubicaciones/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import cargar_municipios
+from .views import cargar_municipios, index
 
 urlpatterns = [
+    path('ajax/cargar-municipios/', cargar_municipios, name='ajax_cargar_municipios'),
+    path('', index, name='index'),
     path('ajax/cargar-municipios/', cargar_municipios, name='ajax_cargar_municipios'),
 ]
